2329f1da46b961cd33d7e0a37dc5de237b9ece71
Renata Jegereva <github@webcreating.co.uk>
2018-02-24 00:14:19 +0000

    feat
    - create BookShelf component to hold lists of books
    - create Book component to populate books list in the future
    - get a list of books from API
    - display each book's title, authors and thumbnail

---
+++ src/BookShelf.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react'
+import Book from './Book'
+import PropTypes from 'prop-types'
+
+class BookShelf extends Component {
+    static propTypes = {
+        books: PropTypes.array.isRequired
+    }
+
+    render() {
+        const { books } = this.props
+
+        let showingBooks
+        showingBooks = books
+
+        return(
+            <div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Currently Reading</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {showingBooks.map((book) => (
+                            <li key={book.id}>
+                                <div className="book">
+                                    <div className="book-top">
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.smallThumbnail})` }}></div>
+                                    <div className="book-shelf-changer">
+                                        <select>
+                                            <option value="none" disabled>Move to...</option>
+                                            <option value="currentlyReading">Currently Reading</option>
+                                            <option value="wantToRead">Want to Read</option>
+                                            <option value="read">Read</option>
+                                            <option value="none">None</option>
+                                        </select>
+                                    </div>
+                                    </div>
+                                    <div className="book-title">{ book.title }</div>
+                                    <div className="book-authors">{ book.authors }</div>
+                                </div>
+                            </li>
+                        ))}
+                    </ol>
+                    </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Want to Read</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book/>
+                        </li>
+                        <li>
+                            <Book/>
+                        </li>
+                    </ol>
+                    </div>
+                </div>
+                <div className="bookshelf">
+                    <h2 className="bookshelf-title">Read</h2>
+                    <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        <li>
+                            <Book/>
+                        </li>
+                        <li>
+                            <Book/>
+                        </li>
+                        <li>
+                            <Book/>
+                        </li>
+                    </ol>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BookShelf;
