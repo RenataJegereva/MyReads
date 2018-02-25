0543a386ad334ab2935e33a4eb4ba395becc421e
Renata Jegereva <github@webcreating.co.uk>
2018-02-25 14:24:57 +0000

    refactor
    housekeeping with variables

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import Book from './Book'
 
 class ListBooks extends Component {
     static propTypes = {
         books: PropTypes.array.isRequired
     }
-
 
     render() {
         const { books } = this.props
@@ -15,46 +15,56 @@ class ListBooks extends Component {
         showingBooks = books
 
         return(
-            <div>
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
-                    <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {showingBooks.filter(book => book.shelf === 'currentlyReading').map((book) => (
-                            <li key={book.id}>
-                                <Book book={ book }  />
-                            </li>
-                        ))}
-                    </ol>
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+
+                <div className="list-books-content">
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Currently Reading</h2>
+                        <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {showingBooks.filter(book => book.shelf === 'currentlyReading').map((book) => (
+                                <li key={book.id}>
+                                    <Book book={ book }  />
+                                </li>
+                            ))}
+                        </ol>
+                        </div>
+                    </div>
+
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Want to Read</h2>
+                        <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {showingBooks.filter(book => book.shelf === 'wantToRead').map((book) => (
+                                <li key={book.id}>
+                                    <Book book={ book } />
+                                </li>
+                            ))}
+                        </ol>
+                        </div>
+                    </div>
+
+                    <div className="bookshelf">
+                        <h2 className="bookshelf-title">Read</h2>
+                        <div className="bookshelf-books">
+                        <ol className="books-grid">
+                            {showingBooks.filter(book => book.shelf === 'read').map((book) => (
+                                <li key={book.id}>
+                                    <Book book={ book } />
+                                </li>
+                            ))}
+                        </ol>
+                        </div>
                     </div>
                 </div>
 
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {showingBooks.filter(book => book.shelf === 'wantToRead').map((book) => (
-                            <li key={book.id}>
-                                <Book book={ book } />
-                            </li>
-                        ))}
-                    </ol>
-                    </div>
+                <div className="open-search">
+                    <Link to='/search'>Add a book</Link>
                 </div>
-
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {showingBooks.filter(book => book.shelf === 'read').map((book) => (
-                            <li key={book.id}>
-                                <Book book={ book } />
-                            </li>
-                        ))}
-                    </ol>
-                    </div>
-                </div>
-            </div>
+          </div>
         )
     }
 }
