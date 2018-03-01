857bcb12dfb82bee6af69e63d0b41a6c08a7c1b5
Renata Jegereva <github@webcreating.co.uk>
2018-03-01 15:24:00 +0000

    refactor: map over shelf names in ListBooks instead of repeating shelf JSX
    - refactor ListBooks into a class instead of functional component
    - add array of shelf names and a function to iterate through them to return a shelf title for html accordingly

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Book from './Book'
+import PropTypes from 'prop-types'
 
 // ListBooks is a functional component and not a class
-function ListBooks(props) {
+class ListBooks extends React.Component {
+  static propTypes = {
+    books: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  }
+
+  shelfTitle(shelfName) {
+    switch(shelfName) {
+      case 'currentlyReading':
+        return 'Currently Reading';
+      case 'wantToRead':
+        return 'Want to read';
+      case 'read':
+        return 'Read';
+      default:
+        return 'none';
+    }
+  }
+
+  render() {
+    const {books, onChangeShelf} =  this.props
+    const shelfNames = ['currentlyReading', 'wantToRead', 'read']
+
     return(
         <div className="list-books">
             <div className="list-books-title">
@@ -11,8 +34,11 @@ function ListBooks(props) {
             </div>
 
             <div className="list-books-content">
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Currently Reading</h2>
+              {shelfNames.map((shelfName, i) => (
+                <div className="bookshelf" key={i}>
+                    <h2 className="bookshelf-title">
+                      { this.shelfTitle(shelfName) }
+                    </h2>
                     <div className="bookshelf-books">
                     <ol className="books-grid">
                         {/* books array received via props from the parent component is filtered to get a new array
@@ -21,40 +47,15 @@ function ListBooks(props) {
                         as a list item that displays individual book's data;
                         Book component is passed down via props a function to change shelf;
                         this function in its own turn is also received via props by ListBooks component from parent App.js; */}
-                        {props.books.filter(book => book.shelf === 'currentlyReading').map((book) => (
+                        {books.filter(book => book.shelf === shelfName).map((book) => (
                             <li key={ book.id }>
-                                <Book book={ book } onChangeShelf={ props.onChangeShelf }  />
+                                <Book book={ book } onChangeShelf={ onChangeShelf }  />
                             </li>
                         ))}
                     </ol>
                     </div>
                 </div>
-
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Want to Read</h2>
-                    <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {props.books.filter(book => book.shelf === 'wantToRead').map((book) => (
-                            <li key={ book.id }>
-                                <Book book={ book } onChangeShelf={ props.onChangeShelf } />
-                            </li>
-                        ))}
-                    </ol>
-                    </div>
-                </div>
-
-                <div className="bookshelf">
-                    <h2 className="bookshelf-title">Read</h2>
-                    <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {props.books.filter(book => book.shelf === 'read').map((book) => (
-                            <li key={ book.id }>
-                                <Book book={ book } onChangeShelf={ props.onChangeShelf } />
-                            </li>
-                        ))}
-                    </ol>
-                    </div>
-                </div>
+              ))}
             </div>
 
             <div className="open-search">
@@ -62,6 +63,7 @@ function ListBooks(props) {
             </div>
         </div>
     )
+  }
 }
 
 export default ListBooks;
